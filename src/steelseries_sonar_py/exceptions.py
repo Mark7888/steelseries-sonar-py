e890e20ff9d95f5175debc59be1f4a85397c9177
Mark7888 <l.mark7888@gmail.com>
2024-01-03 15:54:06 +0100

    exception handling in Sonar with steelseries_sonar_py.exceptions

---
+++ src/steelseries_sonar_py/exceptions.py
@@ -0,0 +1,47 @@
+class EnginePathNotFoundError(Exception):
+    def __str__(self):
+        return "SteelSeries Engine 3 not installed or not in the default location!"
+
+
+class ServerNotAccessibleError(Exception):
+    def __init__(self, status_code):
+        self.status_code = status_code
+
+    def __str__(self):
+        return f"SteelSeries server not accessible! Status code: {self.status_code}"
+
+
+class SonarNotEnabledError(Exception):
+    def __str__(self):
+        return "SteelSeries Sonar is not enabled!"
+
+
+class ServerNotReadyError(Exception):
+    def __str__(self):
+        return "SteelSeries Sonar is not ready yet!"
+
+
+class ServerNotRunningError(Exception):
+    def __str__(self):
+        return "SteelSeries Sonar is not running!"
+
+
+class WebServerAddressNotFoundError(Exception):
+    def __str__(self):
+        return "Web server address not found"
+
+
+class ChannelNotFoundError(Exception):
+    def __init__(self, channel):
+        self.channel = channel
+
+    def __str__(self):
+        return f"Channel '{self.channel}' not found"
+
+
+class InvalidVolumeError(Exception):
+    def __init__(self, volume):
+        self.volume = volume
+
+    def __str__(self):
+        return f"Invalid volume '{self.volume}'! Value must be between 0 and 1!"
--- src/steelseries_sonar_py/sonar.py
+++ src/steelseries_sonar_py/sonar.py
@@ -2,11 +2,19 @@ import requests
 import json
 import os
 
+from . import exceptions as ex
+
 
 class Sonar:
-    # chatCapture -> mic
-    channel_names = [ "master", "game", "chatRender", "chatCapture", "media", "aux" ]
+    # chatCapture = mic
+    channel_names = ["master", "game", "chatRender", "media", "aux", "chatCapture"]
     volume_path = "/volumeSettings/classic"
+    app_data_path = os.path.join(
+        os.environ["ProgramData"],
+        "SteelSeries",
+        "SteelSeries Engine 3",
+        "coreProps.json",
+    )
 
     def __init__(self, streamer_mode=False):
         requests.packages.urllib3.disable_warnings()
@@ -17,69 +25,65 @@ class Sonar:
 
         self.load_base_url()
         self.load_server_address()
-    
+
     def load_base_url(self):
-        common_app_data_path = os.path.join(os.environ["ProgramData"], "SteelSeries", "SteelSeries Engine 3", "coreProps.json")
-        with open(common_app_data_path, "r") as rf:
+        if not os.path.exists(self.app_data_path):
+            raise ex.EnginePathNotFoundError()
+
+        with open(self.app_data_path, "r") as rf:
             common_app_data = json.load(rf)
             self.base_url = f'https://{common_app_data["ggEncryptedAddress"]}'
 
     def load_server_address(self):
         app_data = requests.get(self.base_url + "/subApps", verify=False)
         if app_data.status_code != 200:
-            print(f'SteelSeries server not accessible! Status code: {app_data.status_code}')
-            return False
+            raise ex.ServerNotAccessibleError(app_data.status_code)
+
         app_data_json = json.loads(app_data.text)
 
         if not app_data_json["subApps"]["sonar"]["isEnabled"]:
-            print("SteelSeries Sonar is not enabled!")
-            return False
+            raise ex.SonarNotEnabledError()
 
         if not app_data_json["subApps"]["sonar"]["isReady"]:
-            print("SteelSeries Sonar is not ready!")
-            return False
-        
-        if not app_data_json["subApps"]["sonar"]["isRunning"]:
-            print("SteelSeries Sonar is not running!")
-            return False
+            raise ex.ServerNotReadyError()
 
-        self.web_server_address = app_data_json["subApps"]["sonar"]["metadata"]["webServerAddress"]
+        if not app_data_json["subApps"]["sonar"]["isRunning"]:
+            raise ex.ServerNotRunningError()
+
+        self.web_server_address = app_data_json["subApps"]["sonar"]["metadata"][
+            "webServerAddress"
+        ]
         if self.web_server_address in ["", None, "null"]:
-            print("Sonar webServerAddress not found!")
-            return False
-        
-        print(f'Sonar webServerAddress: {self.web_server_address}')
-        return True
-    
+            raise ex.WebServerAddressNotFoundError()
+
     def get_volume_data(self):
         volume_info_url = self.web_server_address + self.volume_path
         volume_data = requests.get(volume_info_url)
         if volume_data.status_code != 200:
-            print(f'Sonar server not accessible! Status code: {volume_data.status_code}')
-            return {}
+            raise ex.ServerNotAccessibleError(volume_data.status_code)
         volume_data_json = json.loads(volume_data.text)
 
         return volume_data_json
 
     def set_volume(self, channel, volume):
         if channel not in self.channel_names:
-            return {"error": True, "message": f"Channel '{channel}' not found!"}
-        
+            raise ex.ChannelNotFoundError(channel)
+
         if volume < 0 or volume > 1:
-            return {"error": True, "message": f"Invalid volume '{volume}'! Value must be between 0 and 1!"}
-        
-        url = f'{self.web_server_address}{self.volume_path}/{channel}/Volume/{json.dumps(volume)}'
+            raise ex.InvalidVolumeError(volume)
+
+        url = f"{self.web_server_address}{self.volume_path}/{channel}/Volume/{json.dumps(volume)}"
         volume_data = requests.put(url)
 
         return json.loads(volume_data.text)
 
     def mute_channel(self, channel, muted):
         if channel not in self.channel_names:
-            return {"error": True, "message": f"Channel '{channel}' not found!"}
+            raise ex.ChannelNotFoundError(channel)
 
-        muted = (muted == True)
-        
-        url = f'{self.web_server_address}{self.volume_path}/{channel}/Mute/{json.dumps(muted)}'
+        muted = muted == True
+
+        url = f"{self.web_server_address}{self.volume_path}/{channel}/Mute/{json.dumps(muted)}"
         mute_data = requests.put(url)
 
-        return json.loads(mute_data.text)        
+        return json.loads(mute_data.text)
